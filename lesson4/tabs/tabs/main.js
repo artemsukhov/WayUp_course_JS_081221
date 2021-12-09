28b139d49db30c78408ee54f22a943d38405cdd7
artem sukhov <sai43art@gmail.com>
2021-12-09 19:57:38 +0200

    adding tabs

--- lesson4/tabs/tabs/main.js
+++ lesson4/tabs/tabs/main.js
@@ -1,12 +1,27 @@
 const tabs = document.getElementById('tabs');
 const content = document.querySelectorAll('.content');
 
+const tabsDop = document.getElementById('tabs-dop');
+const contentDop = document.querySelectorAll('.content-dop');
+
+
+
+
 const changeClass = el => {
   for(let i = 0; i< tabs.children.length; i++) {
     tabs.children[i].classList.remove('active');
   }
   el.classList.add('active');
 }
+
+const changeClassDop = el => {
+  for(let i = 0; i< tabsDop.children.length; i++) {
+    tabsDop.children[i].classList.remove('active');
+  }
+  el.classList.add('active');
+}
+
+
 
 tabs.addEventListener('click', e => {
   const currTab = e.target.dataset.btn;
@@ -15,6 +30,18 @@ tabs.addEventListener('click', e => {
     content[i].classList.remove('active');
   if(content[i].dataset.content === currTab) {
     content[i].classList.add('active');
+  }
+  }
+
+})
+
+tabsDop.addEventListener('click', e => {
+  const currTabDop = e.target.dataset.btndop;
+  changeClassDop(e.target);
+  for(let i = 0; i< contentDop.length; i++) {
+    contentDop[i].classList.remove('active');
+  if(contentDop[i].dataset.contentdop === currTabDop) {
+    contentDop[i].classList.add('active');
   }
   }
 
